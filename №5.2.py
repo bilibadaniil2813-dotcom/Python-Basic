3f1a3e2779b880bd7cf145feeab352da12e85c06
Gor <bilibadaniil690@gmail.com>
2026-07-27 18:39:23 +0300

    8

---
+++ №5.2.py
@@ -0,0 +1,30 @@
+while True:
+
+    number_1 = int(input("Ведіть перше число: "))
+    mathematical_operation = input("Ведіть математичну операцію: (+, -, *, /) ")
+    number_2 = int(input("Ведіть друге число: "))
+
+    # перевірка на 0  / check for 0
+    if mathematical_operation == "/" and number_2 == 0:
+        print("Дільник не може дорівнювати 0!")
+    else:
+
+    # результат / result  
+        match mathematical_operation:
+            case "+":
+                print("Результат: ", number_1 + number_2)
+            case "-":
+                print("Результат: ", number_1 - number_2)
+            case "*":
+                print("Результат: ", number_1 * number_2)
+            case "/":
+                print("Результат: ", number_1 / number_2)
+            case _:
+                print("Невідома операція")
+
+    #  Питаємо користувача про продовження / We ask the user if they want to continue.
+    continue_calc = input("виконати ще одне обчислення? [y/n]: ").strip().lower()
+
+    if continue_calc not in ('y'):
+        break
+ 
